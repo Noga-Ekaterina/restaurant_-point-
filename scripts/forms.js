3c797bceef00b1ba3d6825b51886be714f29ca7e
katrin210607@gmail.com <katrin210607@gmail.com>
2023-11-30 02:32:17 +0300

    shoe modal (error)

--- scripts/forms.js
+++ scripts/forms.js
@@ -1,4 +1,29 @@
-const BlockShadow= document.querySelector("#block_shadow")
+const blockShadow= document.querySelector("#block_shadow")
+
+class ShowModal{
+	constructor(button, modal, typeButton){
+		this.typeButton= typeButton;
+		this.button= (this.typeButton=="id") ? document.getElementById(button) : document.getElementsByClassName(button);
+		this.modal= document.getElementById(modal)
+	}
+	show(){
+		var Th= this;
+		blockShadow.style.display= "flex"
+		console.log(Th.modal)
+		Th.modal.style.display= "block"
+	}
+	
+	click(){
+		if(this.typeButton=="id"){
+			this.button.onclick= this.show
+		}else{
+			this.button.forEach((el)=>{
+				el.onclick= this.show
+			})
+		}
+	}
+}
+
 class CloseModal{
 	constructor(block){
 		this.block= document.getElementById(block)
@@ -7,9 +32,10 @@ class CloseModal{
 		var Th= this;
 		Th.block.querySelector(".close").onclick=()=>{
 			Th.block.style.display="none";
-			BlockShadow.style.display="none"
+			blockShadow.style.display="none"
 		}
 	}
 }
+new ShowModal("phone", "form_phone", "id").click()
 new CloseModal("form_phone").click()
-new CloseModal("form_Book_an_event").click()
+new CloseModal("form_book_an_event").click()
