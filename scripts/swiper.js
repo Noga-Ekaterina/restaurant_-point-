cb95499a033810af7f0e22d55c9350559566ba8b
Noga-Ekaterina <katrin210607@gmail.com>
2023-12-15 01:25:34 +0300

    #coments

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -8,3 +8,18 @@ var swiper= new Swiper(".slider", {
       delay: 1500,
    },
 })
+var swiperComents= new Swiper("#coments", {
+   spaceBetween: 20,
+   navigation: {
+      nextEl: ".slider-button-next",
+      prevEl: ".slider-button-prev",
+   },
+   breakpoints: {
+      768: {
+         slidesPerView: 2,
+      },
+   },
+   pagination: {
+      el: ".swiper-pagination"
+   },
+})
