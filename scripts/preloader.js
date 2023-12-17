447c0e452078f14389cc8f4e9e0d2e49282c1ed5
Noga-Ekaterina <katrin210607@gmail.com>
2023-12-17 14:47:19 +0300

    hover

--- scripts/preloader.js
+++ scripts/preloader.js
@@ -1,5 +1,6 @@
 const preloader = document.querySelector('#preloader');
 const body = document.body;
+const buttons = document.querySelectorAll('.a_button');
 
 window.onload=()=>{
    preloader.style.opacity= '0'
@@ -7,4 +8,7 @@ window.onload=()=>{
    setTimeout(() => {
       preloader.style.display= "none"
    }, 400);
+   buttons. forEach((btn)=>{
+      btn.dataset.content= btn.innerHTML
+   })
 }
